73128c87b036947c929efc1443009ea314f42f06
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-07-30 19:04:52 -0400

    fix up tests

--- recipe/test_selenium.py
+++ recipe/test_selenium.py
@@ -16,29 +16,29 @@ from selenium.webdriver.firefox.options import Options
 
 import pytest
 
+FIREFOX = Path(sys.prefix) / "bin" / "firefox"
+GECKODRIVER = Path(sys.prefix) / "bin" / "geckodriver"
+
 if "win32" in sys.platform.lower():
     FIREFOX = Path(os.environ["LIBRARY_BIN"]) / "firefox.exe"
     GECKODRIVER = Path(os.environ["SCRIPTS"]) / "geckodriver.exe"
-else:
-    FIREFOX = Path(sys.prefix) / "bin" / "firefox"
-    GECKODRIVER = Path(sys.prefix) / "bin" / "geckodriver"
 
 
 @pytest.mark.parametrize("path,expected_version", [
     [FIREFOX, os.environ["PKG_VERSION"]],
     [GECKODRIVER, None]
 ])
-def test_binary_versions(path, expected_version, allow_call_fail):
+def test_binary_versions(path, expected_version):
     """ assert that the path exists, is callable, and maybe has the right version
     """
     assert path.exists(), "binary not found"
     version = ""
     subprocess.call([str(path), "--version"])
-    proc = subprocess.Popen([str(path), "--version"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    proc = subprocess.Popen([str(path), "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
 
-    for pipe in [stdin, stdout]:
-        version += pipe.read().decode("utf-8") if pipe else ""
+    for pipe in [stdout, stderr]:
+        version += pipe.decode("utf-8") if pipe else ""
 
     assert version.strip(), "no output received"
 
@@ -47,8 +47,8 @@ def test_binary_versions(path, expected_version, allow_call_fail):
 
 def test_read_license(tmp_path):
     geckodriver_log = tmp_path / "geckodriver.log"
-    firefox_log = tmp_path / "firefox.log"
     html_log = tmp_path / "license.html"
+    license_png = tmp_path / "license.png"
 
     print("testing about:license with selenium...")
     driver = None
@@ -56,27 +56,25 @@ def test_read_license(tmp_path):
     try:
         options = Options()
         options.headless = True
+        options.log_level = "trace"
         driver = webdriver.Firefox(options=options,
                                    firefox_binary=str(FIREFOX),
                                    executable_path=str(GECKODRIVER),
-                                   service_log_path=str(geckodriver_log),
-                                   log_path=str(firefox_log))
+                                   service_log_path=str(geckodriver_log))
         driver.get("about:license")
 
         if driver.page_source:
             html_log.write_text(driver.page_source)
             assert "Mozilla Public License 2.0" in driver.page_source, \
                 "couldn't even load the license page"
+            driver.save_screenshot(str(license_png))
+            assert license_png.exists()
     except Exception as err:
         print(f"\nEncountered unexpected error: {type(err)} {err}...\n")
         print(traceback.format_exc())
-        if IGNORE_FIREFOX_FAIL:
-            print("... about:license check ignored because of expected Firefox fail")
-            return_code = 0
-        else:
-            errors += [err]
+        errors += [err]
     finally:
-        errors += list(_dump_logs([firefox_log, geckodriver_log, html_log]))
+        errors += list(_dump_logs([geckodriver_log, html_log]))
 
         if driver:
             driver.quit()
